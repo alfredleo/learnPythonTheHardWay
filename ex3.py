19315dd1c449420341f2ee5b835134d740642b78
Alfred <alfredleo@gmail.com>
2016-08-28 19:26:01 +0500

    ex3, study drill 1. Above each line, use the # to write a comment to yourself explaining what the line does.

--- ex3.py
+++ ex3.py
@@ -1,24 +1,38 @@
+# outputs text
 print "I will now count my chickens:"
 
+# outputs text with addition and division
 print "Hens", 25 + 30 / 6
+# outputs text with subtraction multiplication and mod
 print "Roosters", 100 - 25 * 3 % 4
 
+# outputs text
 print "Now I will count the eggs:"
 
+# outputs long calculations
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# outputs text without any calculations
 print "Is it true that 3 + 2 < 5 - 7"
 
+# outputs boolean value of the expression
 print 3 + 2 < 5 - 7
 
+# outputs addition result with text
 print "What is 3 + 2?", 3 + 2
 
+# outputs subtraction result with text
 print "What is 5 - 7?", 5 - 7
 
+# outputs simple text
 print "Oh, that's why it's False."
 
+# outputs simple text
 print "How about some more."
 
+# outputs text with boolean value on greater than checking
 print "Is it greater?", 5 > -2
+# outputs text with boolean value on greater than or equal checking
 print "is it greater or equal?", 5 >= -2
+# outputs text with boolean value on less than or equal checking
 print "Is it less or equal?", 5 <= -2
