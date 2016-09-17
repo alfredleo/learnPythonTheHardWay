2a82e777441511096e899f3c8e7e3a26a114ab02
Alfred Hamidullin <alfredleo@gmail.com>
2016-09-17 04:18:38 +0000

    requirements.txt now include only new packages. Some passing tests added.

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -7,5 +7,12 @@ class Test(unittest.TestCase):
     def test_simple(self):
         self.assertEqual(prime.simple(7), True)
     
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+        
+    def test_isupper(self):
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
+    
 if __name__ == "__main__":
     unittest.main()
