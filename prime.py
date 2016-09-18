1fe6d3e0fd44be5eb24299d5dc947736aebd3668
Alfred Hamidullin <alfredleo@gmail.com>
2016-09-18 11:03:06 +0000

    prime algorithm and tests

--- prime.py
+++ prime.py
@@ -1,2 +1,6 @@
 def simple(num):
+    if num < 2: return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0: 
+            return False
     return True
--- tests/test_prime.py
+++ tests/test_prime.py
@@ -5,7 +5,14 @@ class Test(unittest.TestCase):
     """Unit tests for prime module."""
     
     def test_simple(self):
-        self.assertEqual(prime.simple(7), True)
+        self.assertTrue(prime.simple(2))
+        self.assertTrue(prime.simple(7))
+        self.assertTrue(prime.simple(13))
+        # 33,265 digits Mersenne prime, 1988 January 29 by Walter Colquitt & Luke Welsh
+        # self.assertTrue(prime.simple(2**110503-1))  # use numpy package
+        self.assertFalse(prime.simple(1729))  # Ramanujan's famous taxi-cab number 7,13,19,91,133,247
+        self.assertFalse(prime.simple(1))
+        self.assertFalse(prime.simple(8))
     
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
