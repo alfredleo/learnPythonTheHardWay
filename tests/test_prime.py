450571e62573791e5aaaafe17fe611801b04f279
Alfred Hamidullin <alfredleo@gmail.com>
2016-09-18 15:21:42 +0000

    some changes

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -5,11 +5,14 @@ class Test(unittest.TestCase):
     """Unit tests for prime module."""
     
     def test_simple(self):
+        # Prime numbers
         self.assertTrue(prime.simple(2))
         self.assertTrue(prime.simple(7))
         self.assertTrue(prime.simple(13))
         # 33,265 digits Mersenne prime, 1988 January 29 by Walter Colquitt & Luke Welsh
         # self.assertTrue(prime.simple(2**110503-1))  # use numpy package
+        
+        # Composite numbers
         self.assertFalse(prime.simple(1729))  # Ramanujan's famous taxi-cab number 7,13,19,91,133,247
         self.assertFalse(prime.simple(1))
         self.assertFalse(prime.simple(8))
