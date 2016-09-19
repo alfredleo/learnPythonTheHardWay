1e32a5fc3fa69b608ed2678183800525f390f86f
Alfred <alfredleo@gmail.com>
2016-09-19 07:31:06 +0500

    some tests for prime and composite numbers

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -7,16 +7,18 @@ class Test(unittest.TestCase):
 
     def test_simple(self):
         # Prime numbers
-        self.assertTrue(prime.simple(2))
-        self.assertTrue(prime.simple(7))
-        self.assertTrue(prime.simple(13))
+        self.assertTrue(prime.is_prime(2))
+        self.assertTrue(prime.is_prime(7))
+        self.assertTrue(prime.is_prime(13))
         # 33,265 digits Mersenne prime, 1988 January 29 by Walter Colquitt & Luke Welsh
-        # self.assertTrue(prime.simple(2**110503-1))  # use numpy package
+        # self.assertTrue(prime.is_prime(2**110503-1))
+        self.assertTrue(prime.is_prime(2 ** 19 - 1))
+        self.assertTrue(prime.is_prime(2 ** 31 - 1))
 
         # Composite numbers
-        self.assertFalse(prime.simple(1729))  # Ramanujan's famous taxi-cab number 7,13,19,91,133,247
-        self.assertFalse(prime.simple(1))
-        self.assertFalse(prime.simple(8))
+        self.assertFalse(prime.is_prime(1729))  # Ramanujan's famous taxi-cab number 7,13,19,91,133,247
+        self.assertFalse(prime.is_prime(1))
+        self.assertFalse(prime.is_prime(8))
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
